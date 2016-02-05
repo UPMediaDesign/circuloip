3fff101618d3b419760c231e58944f5c58c7a529
Carlos <carlospvrgr@gmail.com>
2016-02-05 18:15:10 -0300

    Respaldo Actividades view
    commit

--- wp-content/themes/circulotheme/archive-actividades.php
+++ wp-content/themes/circulotheme/archive-actividades.php
@@ -32,50 +32,13 @@
                     Sede <span class="caret"></span>
                     </button>
                     <ul class="dropdown-menu">
-                        <li><a href="#">La Serena</a></li>
-                        <li><a href="#">República</a></li>
-                        <li><a href="#">San Joaquín</a></li>
-                        <li><a href="#">Rancagua</a></li>
-                        <li><a href="#">Temuco</a></li>
+                        <li><a href="<?php get_bloginfo('url');?>/actividad/?sede=la-serena">La Serena</a></li>
+                        <li><a href="<?php get_bloginfo('url');?>/actividad/?sede=republica">República</a></li>
+                        <li><a href="<?php get_bloginfo('url');?>/actividad/?sede=san-joaquin">San Joaquín</a></li>
+                        <li><a href="<?php get_bloginfo('url');?>/actividad/?sede=rancagua">Rancagua</a></li>
+                        <li><a href="<?php get_bloginfo('url');?>/actividad/?sede=temuco">Temuco</a></li>
                     </ul>
                 </div>
-
-                <div class="col-md-4 col-sm-4 col-xs-12">
-                    <button type="button" class="btn btn-default dropdown-toggle instructor" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Año <span class="caret"></span>
-                    </button>
-                    <ul class="dropdown-menu">
-                        <li><a href="#">2016</a></li>
-                        <li><a href="#">2015</a></li>
-                        <li><a href="#">2014</a></li>
-                        <li><a href="#">2013</a></li>
-                        <li><a href="#">2012</a></li>
-                        <li><a href="#">2011</a></li>
-                        <li><a href="#">2010</a></li>
-                        <li><a href="#">2009</a></li>
-                    </ul>
-                </div>
-
-                <div class="col-md-4 col-sm-4 col-xs-12">
-                    <button type="button" class="btn btn-default dropdown-toggle instructor" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Mes <span class="caret"></span>
-                    </button>
-                    <ul class="dropdown-menu">
-                        <li><a href="">Enero</a></li>
-                        <li><a href="">Febrero</a></li>
-                        <li><a href="">Marzo</a></li>
-                        <li><a href="">Abril</a></li>
-                        <li><a href="">Mayo</a></li>
-                        <li><a href="">Junio</a></li>
-                        <li><a href="">Julio</a></li>
-                        <li><a href="">Agosto</a></li>
-                        <li><a href="">Septiembre</a></li>
-                        <li><a href="">Octubre</a></li>
-                        <li><a href="">Noviembre</a></li>
-                        <li><a href="">Diciembre</a></li>
-                    </ul>
-                </div>
-
 
         </div>
     </div>
@@ -84,7 +47,11 @@
 <section class="actividades">
     <div class="container">
         <div class="row">
-                    <?php $timeline = get_posts(array('post_type' => 'actividades', 'meta_query' => array(array('key' => 'sede', 'value' => $_GET['sede'], 'compare' => 'LIKE') ), ) )?>
+                    <?php if($_GET['sede']){
+                        $posts = get_posts(array(
+                            'post_type' => 'actividades', 'meta_query' => array(array('key' => 'sede', 'value' => $_GET['sede'], 'compare' => 'LIKE') ), ) );
+                    } ?>
+
                     <?php $countactividades = 0?>
                     <?php foreach($posts as $actividad):?>
                     <?php $countactividades++?>
@@ -96,6 +63,7 @@
                             $actsocial = 'soc-visible';
                             $actcolimg = 'heading col-md-12 col-sm-12 col-xs-12';
                             $actcolcaption = 'col-md-12 col-sm-12 col-xs-12';
+                            $actseparator = '<div style="margin-bottom:30px;"></div>';
                         }
 
                         else{
@@ -112,12 +80,34 @@
                                 <span class="day"><?php echo the_time('d')?></span>
                                 <span class="month"><?php echo the_time('M')?></span>
                             </div>
-                            <a class="<?php echo $actcolimg ?>" href="<?php echo get_permalink($actividad->ID)?>"><?php echo get_the_post_thumbnail($actividad->ID , $actsize , array('class' => 'img-responsive'))?></a>
+                            <a class="<?php echo $actcolimg?>" href="<?php echo get_permalink($actividad->ID)?>"><?php echo get_the_post_thumbnail($actividad->ID , $actsize , array('class' => 'img-responsive'))?></a>
                             <figcaption class="<?php echo $actcolcaption ?>">
                                 <h4><a href="<?php echo get_permalink($actividad->ID)?>" ><?php echo $actividad->post_title?></a></h4>
                                 <p><?php echo substr($actividad->post_content , 0, 65)?>...</p>
                                 
-                                <a class="vermas" href="<?php echo get_permalink($actividad->ID)?>" title="Ver más" rel="blog">Ver más <i class="fa fa-arrow-right"></i></a>
+                                <?php $ss = get_field('sede', $actividad->ID)?>
+                                <?php if($ss == 'la-serena'){
+                                    $lugar = 'La Serena';
+                                    }
+                                elseif($ss == 'republica'){
+                                    $lugar = 'República';
+                                    }
+                                elseif($ss == 'san-joaquin'){
+                                    $lugar = 'San Joaquín';
+                                    }
+                                elseif($ss == 'rancagua'){
+                                    $lugar = 'Rancagua';
+                                    }
+                                elseif($ss == 'temuco'){
+                                    $lugar = 'Temuco';
+                                    }
+                                ?>
+                                
+                                <section class="asignment">
+                                    <a class="vermas" href="<?php echo get_permalink($actividad->ID)?>" title="Ver más" rel="blog">Ver más <i class="fa fa-arrow-right"></i></a>
+                                    <p class="sede"><span class="fa fa-map-marker"></span> Sede <?php echo $lugar?>
+                                    </p>
+                                </section>
 
                                 <ul class="<?php echo $actsocial?>">
                                         <li><a onclick="basicPopup(this.href);return false" target="_blank" href="https://www.linkedin.com/shareArticle?mini=true&url=<?php the_permalink();?>&title=<?php the_title(); ?>&summary=&source="><i class="fa fa-linkedin"></i></a></li>
@@ -126,7 +116,9 @@
                                 </ul>
                                 <div class="separator"></div>
                             </figcaption>
+                            <?php echo $actseparator?>
                         </figure>
+
                     
                     <?php endforeach?>                
                     <div class="clear separator"></div>
